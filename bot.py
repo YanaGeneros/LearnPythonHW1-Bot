2c697763b6b2f50f75c8ad5dadb807b309da4bb5
Yana <yana.generos@gmail.com>
2025-03-01 11:21:26 +0400

    Save changes before switching

--- bot.py
+++ bot.py
@@ -1,43 +1,30 @@
 import logging
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Application, CallbackQueryHandler, CommandHandler, MessageHandler, filters, CallbackContext, ContextTypes
+from telegram.ext import Application, CallbackQueryHandler, CommandHandler, MessageHandler, CallbackContext, ContextTypes
 
 import Settings
 
 #enable logging
 logging.basicConfig(filename = "bot.log", level = logging.INFO)
 
-async def greet_user(update: Update, context: CallbackContext):
-    print("вызван /start")
-    await update.message.reply_text("Привет, друг!")
-
-async def talk_to_me(update: Update, context: CallbackContext):
-     text = update.message.text
-     print(text)
-     await update.message.reply_text(text)
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [
-
         [
-            InlineKeyboardButton("Option 1", callback_data="1"),
-            InlineKeyboardButton("Option 2", callback_data="2"),
+            InlineKeyboardButton("Click 1", callback_data="1"),
+            InlineKeyboardButton("click 2", callback_data="Hello, world" ),
         ],
-        [InlineKeyboardButton("Option 3", callback_data="3")],
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    await update.message.reply_text("please choose", reply_markup=reply_marcup)
+    await update.message.reply_text("Hello, dude! Choose option", reply_markup=reply_markup)
 
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Parses the CallbackQuery and updates the message text."""
+async def button(update: Update, context: ContextTypes. DEFAULT_TYPE) -> None:
     query = update.callback_query
 
-    #CallbackQueries need to be answered, even if no notofication to the user is needed
     await query.answer()
-    await query.edit_message_text(text=f"selected option: {query.data}")
+    await query.edit_message_text(text=f"selected option:{query.data}")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Displays info on how to use the bot."""
@@ -45,15 +32,13 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
 def main() -> None:
     """Run the bot."""
-
     mybot = Application.builder().token(Settings.APi_KEY).build()
 
     mybot.add_handler(CommandHandler("start", start))
-    mybot.add_handler(CallbackQueryHandler(button))
     mybot.add_handler(CommandHandler("help", help_command))
+    mybot.add_handler(CallbackQueryHandler(button))
 
     mybot.run_polling(allowed_updates=Update.ALL_TYPES)
-
 
 if __name__ == "__main__":
     main()
